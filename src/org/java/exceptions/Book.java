19277dea76d207713c992a3c3fcb1d084d4fdf54
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-15 18:13:10 +0100

    added data on Library class

--- src/org/java/exceptions/Book.java
+++ src/org/java/exceptions/Book.java
@@ -2,19 +2,19 @@ package org.java.exceptions;
 
 public class Book {
 
-	public String title;
-	public int pageNumber;
-	public String author;
-	public String publisher;
+	private String title;
+	private int pageNumber;
+	private String author;
+	private String publisher;
 	
 	//CONSTRUCTOR
 	
-	public Book(String title, int pageNumber, String author, String publisher) {
+	public Book(String title, int pageNumber, String author, String publisher) throws Exception {
 		
-		this.title = title;
-		this.pageNumber = pageNumber;
-		this.author = author;
-		this.publisher = publisher;
+        setTitle(title);
+        setPageNumber(pageNumber);
+        setAuthor(author);
+        setPublisher(publisher);
 
 	}
 	
@@ -25,44 +25,54 @@ public class Book {
 		return title;
 	}
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) throws Exception {
+        if (title == null || title.trim().isEmpty()) {
+            throw new Exception("Il titolo non può essere vuoto.");
+        }
+        this.title = title;
+    }
 
 	public int getPageNumber() {
 		return pageNumber;
 	}
 
-	public void setPageNumber(int pageNumber) {
-		this.pageNumber = pageNumber;
-	}
+    public void setPageNumber(int pageNumber) throws Exception {
+        if (pageNumber <= 0) {
+            throw new Exception("Il numero di pagine deve essere maggiore di zero.");
+        }
+        this.pageNumber = pageNumber;
+    }	
 
 	public String getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
+    public void setAuthor(String author) throws Exception {
+        if (author == null || author.trim().isEmpty()) {
+            throw new Exception("L'autore non può essere vuoto.");
+        }
+        this.author = author;
+    }
 
 	public String getPublisher() {
 		return publisher;
 	}
 
-	public void setPublisher(String publisher) {
-		this.publisher = publisher;
-	}
+    public void setPublisher(String publisher) throws Exception {
+        if (publisher == null || publisher.trim().isEmpty()) {
+            throw new Exception("L'editore non può essere vuoto.");
+        }
+        this.publisher = publisher;
+    }
 	
 	
-	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return "Libro" + "/n" +
-				"Titolo=" + title + "/n" +
-		        "Pagine=" + pageNumber + "/n" +
-				"Autore=" + author + "/n" +
-		        "Editore=" + publisher;
-	}
-		
-	
+	    return "Libro\n" +
+	            "Titolo= " + title + "\n" +
+	            "Pagine= " + pageNumber + "\n" +
+	            "Autore= " + author + "\n" +
+	            "Editore= " + publisher + "\n" +
+	            "\n---------------------\n";
+	}	
 }
+
--- src/org/java/exceptions/Library.java
+++ src/org/java/exceptions/Library.java
@@ -1,5 +1,43 @@
 package org.java.exceptions;
 
-public class Library {
+import java.util.Scanner;
 
+public class Library {
+    public static void main(String[] args) throws Exception {
+        try (Scanner in = new Scanner(System.in)) {
+			System.out.print("Inserisci il numero di libri: ");
+			int n = in.nextInt();
+
+			Book[] books = new Book[n];
+
+			for (int i = 0; i < n; i++) {
+			    try {
+			        System.out.println("Inserisci i dati per il libro " + (i + 1) + ":");
+			        System.out.print("Titolo: ");
+			        String title = in.next();
+
+			        System.out.print("Numero di pagine: ");
+			        int pageNumber = in.nextInt();
+
+			        System.out.print("Autore: ");
+			        String author = in.next();
+
+			        System.out.print("Editore: ");
+			        String publisher = in.next();
+
+			        Book b = new Book(title, pageNumber, author, publisher);
+
+
+			        books[i] = b;
+
+			        System.out.println("Libro inserito con successo: " + "\n" + b);
+			    } catch (Exception e) {
+			        System.out.println("Errore: " + e.getMessage());
+			        i--; 
+			    }
+			}
+			System.out.println("Elenco dei libri inseriti: ");
+			System.out.println(books.toString());
+		}
+    }
 }
