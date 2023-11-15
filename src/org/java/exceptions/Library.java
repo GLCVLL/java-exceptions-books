b1de600089536a62bf96a5c10243d0972a2432ad
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-15 23:15:54 +0100

    added method to write and read a file

--- src/org/java/exceptions/Library.java
+++ src/org/java/exceptions/Library.java
@@ -1,8 +1,15 @@
 package org.java.exceptions;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Library {
+	
+	static final File test = new File("C:\\test.txt");
+	
     public static void main(String[] args) throws Exception {
         try (Scanner in = new Scanner(System.in)) {
 			System.out.print("Inserisci il numero di libri: ");
@@ -35,6 +42,55 @@ public class Library {
 			        System.out.println("Errore: " + e.getMessage());
 			        i--; 
 			    }
+			}
+			
+			//WRITE
+			
+			FileWriter myWriter = null;
+
+			try {
+			    myWriter = new FileWriter(test);
+
+			    
+			    for (int x=0;x<books.length;x++) {
+			        myWriter.write(books[x].toString());
+			    }
+
+			} catch (IOException e) {
+			    e.printStackTrace();
+			} finally {
+			    try {
+			        if (myWriter != null) {
+			            myWriter.close();
+			        }
+			    } catch (IOException e) {
+			        e.printStackTrace();
+			    }
+			}
+
+			System.out.println("Elenco dei libri inseriti: ");
+			for (int x=0;x<books.length;x++) {
+			    System.out.println(books[x]);
+			}
+			
+			// READ
+			
+			Scanner reader = null;
+			try {
+				
+				reader = new Scanner(test);
+				
+				while (reader.hasNextLine()) {
+				   String data = reader.nextLine();
+				   System.out.println(data);
+				}
+			} catch (FileNotFoundException e) {
+				
+				System.out.println("Error reading file: " + e.getMessage());
+			} finally {
+				
+				if (reader != null)
+					reader.close();
 			}
 			System.out.println("Elenco dei libri inseriti: ");
 			System.out.println(books.toString());
