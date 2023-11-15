a35efc490e3d570012f0d0898ecf3ba3b94ae06c
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-15 16:55:40 +0100

    added Book and Library classes

--- src/org/java/exceptions/Book.java
+++ src/org/java/exceptions/Book.java
@@ -2,4 +2,67 @@ package org.java.exceptions;
 
 public class Book {
 
+	public String title;
+	public int pageNumber;
+	public String author;
+	public String publisher;
+	
+	//CONSTRUCTOR
+	
+	public Book(String title, int pageNumber, String author, String publisher) {
+		
+		this.title = title;
+		this.pageNumber = pageNumber;
+		this.author = author;
+		this.publisher = publisher;
+
+	}
+	
+	// GETTER & SETTER
+
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPageNumber() {
+		return pageNumber;
+	}
+
+	public void setPageNumber(int pageNumber) {
+		this.pageNumber = pageNumber;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Libro" + "/n" +
+				"Titolo=" + title + "/n" +
+		        "Pagine=" + pageNumber + "/n" +
+				"Autore=" + author + "/n" +
+		        "Editore=" + publisher;
+	}
+		
+	
 }
